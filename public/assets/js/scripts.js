174312700b4ce4e3026511414154d2c21120265c
andreseguanipa1 <andreseguanipa1@gmail.com>
2021-03-28 14:13:33 -0430

    Tercer commit

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -5,7 +5,7 @@ function displayProducts(productos) {
     productos.forEach(element => {
 
         productsHTML +=
-            `<div class='product-container'>
+            `<div data-aos="fade-up" class='product-container'>
         <h4 class='name'>${element.name}</h4>
         <img src=${element.img}>
         <h3 class='price'>Precio: ${element.price}$</h3>
@@ -17,9 +17,13 @@ function displayProducts(productos) {
     document.getElementById('page-content').innerHTML = productsHTML;
 }
 
+function aosAnimations() {
+    AOS.init();
+}
+
 window.onload = async() => {
     const productList = await (await fetch("/database")).json();
+    aosAnimations();
     console.log(productList);
     displayProducts(productList);
-
 }
